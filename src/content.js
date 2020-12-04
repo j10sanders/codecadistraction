5546ae01965df9918d4b8104b4b740bc525f8dc6
Jonathan <jsanders@codecademy.com>
2020-12-04 08:42:28 -0500

    bit of cleanup

--- src/content.js
+++ src/content.js
@@ -5,47 +5,9 @@ import ReactDOM from "react-dom";
 import Frame, { FrameContextConsumer } from "react-frame-component";
 import App from "./App";
 
-// export const getCurrentTabUrl = (callback) => {
-//   console.log("tab");
-//   const queryInfo = { active: true, lastFocusedWindow: true };
-//   console.log(chrome.tabs, "chrome.tabs", chrome);
-//   chrome.tabs &&
-//     chrome.tabs.query(queryInfo, (tabs) => {
-//       console.log(tabs[0], "tabs of 0");
-//       callback(tabs[0].url);
-//     });
-// };
-
 const Main = () => {
-  // const [display, setDisplay] = useState("block");
-  // useEffect(() => {
-  //   console.log("effect");
-  //   getCurrentTabUrl((url) => {
-  //     // setUrl(url || "undefined");
-  //     console.log(url, "URL");
-  //     if (url === "https://www.netflix.com/browse") {
-  //       console.log("IT IS NETFLIX");
-  //       setDisplay("block");
-  //       // chrome.tabs.update({ url: "https://codecademy.com" });
-  //     }
-  //   });
-  // }, []);
-  // chrome.runtime.onMessage.addListener(function (
-  //   request,
-  //   sender,
-  //   sendResponse
-  // ) {
-  //   console.log(
-  //     sender.tab
-  //       ? "from a content script:" + sender.tab.url
-  //       : "from the extension"
-  //   );
-  //   // if (request.url == "hello") sendResponse({ farewell: "goodbye" });
-  //   console.log(request, "REQWUREHILUFAIHLUAEHGIUY");
-  // });
   return (
     <Frame
-      // style={{ display: display }}
       head={[
         <link
           type="text/css"
@@ -56,14 +18,7 @@ const Main = () => {
     >
       <FrameContextConsumer>
         {({ document, window }) => {
-          return (
-            <App
-              document={document}
-              window={window}
-              isExt={true}
-              // style={{ display: display }}
-            />
-          );
+          return <App document={document} window={window} isExt={true} />;
         }}
       </FrameContextConsumer>
     </Frame>
