c896f9e9ef3f0b25f17e8b9f3eb908fb8f896728
linkcoding <kenny.lin.91@gmail.com>
2020-12-04 12:44:41 -0500

    api

--- src/content.js
+++ src/content.js
@@ -69,33 +69,42 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
         );
       }
     });
+  }
+  
   if (request.tab === "https://www.codecademy.com/learn" ) {
     // Use to convert .getDay's return number to match the index in listed days (everything's off by 1 since .getDay() has Sunday as 0)
     console.log('AT CODECADEMY')
-    const daysToIndices = { 0: 6, 1: 0, 2: 1, 3: 2, 4: 3, 5: 4, 6: 5 }
+    const indexToDays = { 0: 1, 1: 2, 2: 3, 3: 4, 4: 5, 5: 6, 6: 0 }
     const weekEl = document.getElementById('target-week')
     const dayEls = [...weekEl.children]
-    const today = new Date().getDay()
-    let streakIndicator = dayEls[daysToIndices[today]].ariaLabel
-    
-    document.getElementById('target-counter')​
-    vals = document.getElementById('target-counter').children
-    vals[0].innerHTML
-    vals[1].innerHTML.match(/\d/)[0]
+    const daysCompleted = dayEls.reduce((a, c, idx) => {
+      return c.ariaLabel ? [...a, indexToDays[idx]] : [...a]
+    }, [])
 
-
-    if(streakIndicator !== "") {
-      chrome.storage.local.set(
-        { lastValidCompletion: new Date().toDateString() },
-        function () {
-          console.log("Value is set to " + new Date());
-        }
-      );
+    function checkTodaysCompletion() {
+      const today = new Date().getDay()
+      return daysCompleted[daysCompleted.length - 1] === today
     }
+    
+    const counterValues = document.getElementById('target-counter').children
+    const target = Number(counterValues[0].innerHTML)
+    const goal = Number(counterValues[1].innerHTML.match(/\d/)[0])
+    const weeklyStreakCompleted = target >= goal
+
+    const stats = {
+      daysCompleted: daysCompleted,
+      todayCompleted: checkTodaysCompletion(),
+      weeklyStreakCompleted: weeklyStreakCompleted
+    } 
+
+    chrome.storage.local.set({ lastValidCompletion: stats }, function () {
+        console.log("Value is set to " + stats.daysCompleted, stats.todayCompleted, stats.weeklyStreakCompleted);
+      }
+    );
+    
   }
   console.log(request.greeting, "GREETING");
   console.log(request, "REQUESRT");
-  }
 });
 
 // function toggle() {
