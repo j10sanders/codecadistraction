baf9daa18f7461877136c98e56f361ec230b51f6
Jonathan <jsanders@codecademy.com>
2020-12-04 13:32:56 -0500

    eh

--- src/App.js
+++ src/App.js
@@ -65,7 +65,7 @@ const App = ({ codeLater, setCodeLater, data }) => {
               </Col>
               <Col>
                 <CTAButton href="https://codecademy.com/learn" mode="light">
-                  Code First :)
+                  I'll Code First ❤️
                 </CTAButton>
               </Col>
             </Row>
